63d9d37715b66612b4e8bcbdd5f3250c1e5ff666
Bryan Perez <bsperezgo@gmail.com>
2022-01-27 16:33:57 -0500

    feat: default error middleware

--- app_test.go
+++ app_test.go
@@ -2,6 +2,7 @@ package HandlerFramework
 
 import (
 	"context"
+	"errors"
 	"log"
 	"reflect"
 	"testing"
@@ -29,6 +30,44 @@ func TestOkChainedHandlers(t *testing.T) {
 	handler3 := func(req *HandlerRequest, res *HandlerResponse, next NextFunction) {
 		log.Println("[INFO] Third call changing the response")
 		res.StatusCode = 200
+	}
+	mainHandler := ComposeHandlers(handler1, handler2, handler3)
+	t.Run(testName, func(t *testing.T) {
+		log.Println("[INFO] TEST NAME:", testName)
+		ctx := context.Background()
+		resGotten := mainHandler.Handle(ctx, reqValue)
+		if !reflect.DeepEqual(resGotten, resValue) {
+			t.Errorf("The response gotten %+v, is not equal to response expected %+v", resGotten, resValue)
+		}
+	})
+}
+
+func TestChainWithThreeHandlersAndInTheSecondGetError(t *testing.T) {
+	testName := "should return error in the second handler, and call the error middleware"
+	type body struct {
+		value string
+	}
+	reqValue := HandlerRequest{
+		Body: body{value: "value"},
+	}
+	resValue := HandlerResponse{
+		StatusCode: 400,
+		Message:    "Some error ocurred",
+	}
+	handler1 := func(req *HandlerRequest, res *HandlerResponse, next NextFunction) {
+		log.Println("[INFO] First call doing something")
+		res.StatusCode = 200
+		next()
+	}
+	handler2 := func(req *HandlerRequest, res *HandlerResponse, next NextFunction) {
+		log.Println("[INFO] Second call doing something")
+		res.StatusCode = 201
+		err := errors.New("Some error ocurred")
+		next(err)
+	}
+	handler3 := func(req *HandlerRequest, res *HandlerResponse, next NextFunction) {
+		log.Println("[INFO] Third call changing the response, This log wont be printed")
+		res.StatusCode = 202
 	}
 	mainHandler := ComposeHandlers(handler1, handler2, handler3)
 	t.Run(testName, func(t *testing.T) {
--- errorMiddleware.go
+++ errorMiddleware.go
@@ -2,8 +2,10 @@ package HandlerFramework
 
 import "log"
 
-type IErrorMiddleware func(err error, req interface{}, res interface{})
+type IErrorMiddleware func(err error, req *HandlerRequest, res *HandlerResponse)
 
-func defaultErrorMiddleware(err error, req interface{}, res interface{}) {
+func defaultErrorMiddleware(err error, req *HandlerRequest, res *HandlerResponse) {
 	log.Println("[ERROR] Calling middleware error with:", err)
+	res.StatusCode = 400
+	res.Message = err.Error()
 }
